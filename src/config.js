f8382775fe042977e6d537e904f0ffdf45c6fc2f
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2020-05-18 15:25:13 -0400

    add token route, clean code, update config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development'
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    TWILIO_API_KEY: process.env.TWILIO_API_KEY,
+    TWILIO_API_SECRET: process.env.TWILIO_API_SECRET
 }
--- src/routes/twilio-token/twilio-token-router.js
+++ src/routes/twilio-token/twilio-token-router.js
@@ -1,1 +1,33 @@
 const express = require('express')
+const tokenRouter = express.Router()
+const { TWILIO_API_KEY, TWILIO_API_SECRET } = require('../../config')
+const { AccessToken } = require('twilio').jwt
+const { VideoGrant } = AccessToken
+const twilioAccountSid = 'AC712594f590c0d874685c04858f7398f9' // Your Account SID from www.twilio.com/console
+// const TokenService = require('../services/tokenService');
+
+// POST /token
+tokenRouter.post('/', (req, res) => {
+  // Create an access token which we will sign and return to the client,
+  // containing the grant we just created
+  const token = new AccessToken(
+    twilioAccountSid,
+    TWILIO_API_KEY,
+    TWILIO_API_SECRET
+  )
+  token.identity = req.body.myUserId
+
+  // Create a Video grant which enables a client to use Video
+  // and limits access to the specified Room (DailyStandup)
+  const videoGrant = new VideoGrant({
+    room: req.body.partnerX,
+  })
+
+  // Add the grant to the token
+  token.addGrant(videoGrant)
+
+  // Serialize the token to a JWT string
+  res.status(200).send(JSON.stringify({ token: token.toJwt() }))
+})
+
+module.exports = tokenRouter
