c366e3d0886723ecdac569f5398faee49e7902bc
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2020-06-11 15:46:17 -0400

    add some default values for runEvent

--- src/routes/rooms/runEvent.js
+++ src/routes/rooms/runEvent.js
@@ -13,9 +13,9 @@ let currentRound = 0
 const runEvent = async (req, res) => {
   console.log('runEvent ran')
   const eventId = req.params.id
-  const numRounds = req.body.num_rounds
-  const roundLength = req.body.round_length
-  const roundInterval = req.body.round_interval || 10000
+  const numRounds = req.body.num_rounds || 10 // default ten rounds
+  const roundLength = req.body.round_length || 500000 // default 5 minute rounds
+  const roundInterval = req.body.round_interval || 15000 // default 15 second interval
 
   // ensures that rooms are closed before next round
   try {
