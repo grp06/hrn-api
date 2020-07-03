74474b04f1973b3d8e8bd649b298551e4806e0b6
George Pickett <gpickett00@gmail.com>
2020-07-03 15:07:31 -0400

    Remove feature that updates last_seen from backend

--- src/gql/queries/users/getOnlineUsersByEventId.js
+++ src/gql/queries/users/getOnlineUsersByEventId.js
@@ -3,11 +3,11 @@ import gql from 'graphql-tag'
 export const getOnlineUsersByEventId = gql`
   query getOnlineUsersByEventId($later_than: timestamptz, $event_id: Int) {
     event_users(
-      where: { user: { last_seen: { _gte: $later_than } }, _and: { event_id: { _eq: $event_id } } }
+      where: { user: { updated_at: { _gte: $later_than } }, _and: { event_id: { _eq: $event_id } } }
     ) {
       user {
         id
-        last_seen
+        updated_at
         name
       }
     }
--- src/routes/auth/auth-router.js
+++ src/routes/auth/auth-router.js
@@ -1,25 +1,12 @@
 const express = require('express')
 import orm from '../../services/orm'
 import findUserByEmail from '../../gql/queries/users/findUserByEmail'
-import updateLastSeen from '../../gql/mutations/users/updateLastSeen'
 import { createToken } from '../../extensions/jwtHelper'
 import { comparePasswords } from '../../services/auth-service'
 import * as Sentry from '@sentry/node'
 
 const authRouter = express.Router()
 const jsonBodyParser = express.json()
-
-authRouter.post('/update-last-seen', jsonBodyParser, async (req, res) => {
-  try {
-    const now = new Date().toISOString()
-    const ls = await orm.request(updateLastSeen, { now, id: req.body.userId })
-  } catch (error) {
-    console.log('error', error)
-    return res.status(500).json({ error: 'error updating last seen' })
-  }
-
-  return res.status(201).json({ message: 'updated last seen' })
-})
 
 authRouter.post('/login', jsonBodyParser, async (req, res, next) => {
   const { email, password } = req.body
