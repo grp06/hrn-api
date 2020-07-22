f286ff0d537cb877e9df4d8eb7f8869b6c0ea8e1
George Pickett <gpickett00@gmail.com>
2020-07-21 22:56:07 -0400

    20 second rounds

--- src/routes/rooms/runEvent.js
+++ src/routes/rooms/runEvent.js
@@ -21,7 +21,7 @@ const runEvent = async (req, res) => {
   const numRounds = req.body.num_rounds || 10 // default ten rounds
   const round_length = req.body.round_length * oneMinuteInMs || 300000 // default 5 minute rounds
 
-  const roundInterval = req.body.round_interval || 35000 // default 35 second interval
+  const roundInterval = req.body.round_interval || 20000 // default 35 second interval
 
   if (req.body.reset) {
     console.log('resetting event')
