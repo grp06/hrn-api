52e2106e1485adacebd6f2194ea320d24f12834e
George Pickett <gpickett00@gmail.com>
2020-08-31 18:49:41 -0400

    Add new check for resetting

--- src/routes/rooms/runEvent.js
+++ src/routes/rooms/runEvent.js
@@ -7,6 +7,7 @@ import orm from '../../services/orm'
 import { omniFinishRounds, endEvent, resetEvent } from './runEventHelpers'
 import updateEventObject from '../../gql/mutations/event/updateEventObject'
 import getOnlineUsers from './getOnlineUsers'
+import getEventStatusByEventId from '../../gql/queries/getEventStatusByEventId'
 
 const runEvent = async (req, res, currentRound = 0, betweenRoundsTimeout, roundsTimeout) => {
   const oneMinuteInMs = 60000
@@ -20,122 +21,134 @@ const runEvent = async (req, res, currentRound = 0, betweenRoundsTimeout, rounds
   if (req.body.reset) {
     await resetEvent(eventId, betweenRoundsTimeout, roundsTimeout)
 
-    currentRound = 0
     console.log('reset event complete,')
     console.log('eventId = ', eventId)
 
     return
   }
-  // ensures that rooms are closed before next round
+
+  let eventStatus
   try {
-    await omniFinishRounds(currentRound, eventId)
+    const eventStatusResponse = await getEventStatusByEventId(eventId)
+    eventStatus = eventStatusResponse.data.events.status
+    console.log('runEvent -> eventStatus', eventStatus)
   } catch (error) {
-    Sentry.captureException(error)
     console.log(error)
+    Sentry.captureException(error)
   }
 
-  // end event if numRounds reached
-  if (parseInt(currentRound, 10) === parseInt(numRounds, 10)) {
-    console.log('reached last round, going to end event')
-
-    setTimeout(() => {
-      endEvent(eventId, betweenRoundsTimeout, roundsTimeout)
-    }, roundInterval / 2)
-    return
-  }
-
-  // to be used for timeout function
-  const delayBetweenRounds = currentRound === 0 ? 0 : roundInterval
-
-  // big function defining what to do during each round
-  // first round it executes immediately. Otherwise its every ${roundInterval} secs
-  betweenRoundsTimeout = setTimeout(async () => {
-    let onlineEventUsers
+  if (eventStatus !== 'not-started' || eventStatus !== 'complete') {
+    // ensures that rooms are closed before next round
     try {
-      onlineEventUsers = await getOnlineUsers(eventId)
-    } catch (error) {
-      console.log('error = ', error)
-      Sentry.captureException(error)
-      return clearTimeout(roundsTimeout)
-    }
-
-    // get data for rounds
-    let roundsData
-
-    try {
-      const getRoundsResponse = await orm.request(getRoundsByEventId, { event_id: eventId })
-      roundsData = getRoundsResponse.data
+      await omniFinishRounds(currentRound, eventId)
     } catch (error) {
       Sentry.captureException(error)
-      return clearTimeout(roundsTimeout)
+      console.log(error)
     }
 
-    // create an array of pairings for a given round/event for use in algorithm
-    const variablesArr = []
-    const roundsMap = createRoundsMap(roundsData, onlineEventUsers)
+    // end event if numRounds reached
+    if (parseInt(currentRound, 10) === parseInt(numRounds, 10)) {
+      console.log('reached last round, going to end event')
 
-    const { pairingsArray: newPairings } = samyakAlgoPro(onlineEventUsers, roundsMap)
-    console.log('betweenRoundsTimeout -> newPairings', newPairings)
-    console.log('eventId = ', eventId)
-    // do something to check for NULL matches or if game is over somehow
-    // -------------------------------mutation to update eventComplete (ended_at in db)
-
-    const numNullPairings = newPairings.reduce((all, item, index) => {
-      if (item.indexOf(null) > -1) {
-        all += 1
-      }
-      return all
-    }, 0)
-
-    if (newPairings.length === 0 || numNullPairings > onlineEventUsers.length / 2) {
-      return setTimeout(() => {
+      setTimeout(() => {
         endEvent(eventId, betweenRoundsTimeout, roundsTimeout)
       }, roundInterval / 2)
+      return
     }
 
-    // insert data for given round
-    // maybe a .map would be cleaner here?
-    newPairings.forEach((pairing) => {
-      variablesArr.push({
-        partnerX_id: pairing[0],
-        partnerY_id: pairing[1],
-        round_number: currentRound + 1,
-        event_id: eventId,
-      })
-    })
+    // to be used for timeout function
+    const delayBetweenRounds = currentRound === 0 ? 0 : roundInterval
 
-    // insert new pairings result into db
-    try {
-      await orm.request(bulkInsertRounds, {
-        objects: variablesArr,
-      })
-    } catch (error) {
-      Sentry.captureException(error)
-      console.log('error inserting rounds data = ', error)
-      clearTimeout(roundsTimeout)
-    }
+    // big function defining what to do during each round
+    // first round it executes immediately. Otherwise its every ${roundInterval} secs
+    betweenRoundsTimeout = setTimeout(async () => {
+      let onlineEventUsers
+      try {
+        onlineEventUsers = await getOnlineUsers(eventId)
+      } catch (error) {
+        console.log('error = ', error)
+        Sentry.captureException(error)
+        return clearTimeout(roundsTimeout)
+      }
 
-    // increment current round in events table
-    try {
-      await orm.request(updateEventObject, {
-        id: eventId,
-        newCurrentRound: currentRound + 1,
-        newStatus: 'room-in-progress',
-      })
-      console.log('set room to in-progress')
+      // get data for rounds
+      let roundsData
+
+      try {
+        const getRoundsResponse = await orm.request(getRoundsByEventId, { event_id: eventId })
+        roundsData = getRoundsResponse.data
+      } catch (error) {
+        Sentry.captureException(error)
+        return clearTimeout(roundsTimeout)
+      }
+
+      // create an array of pairings for a given round/event for use in algorithm
+      const variablesArr = []
+      const roundsMap = createRoundsMap(roundsData, onlineEventUsers)
+
+      const { pairingsArray: newPairings } = samyakAlgoPro(onlineEventUsers, roundsMap)
+      console.log('betweenRoundsTimeout -> newPairings', newPairings)
       console.log('eventId = ', eventId)
-    } catch (error) {
-      Sentry.captureException(error)
-    }
+      // do something to check for NULL matches or if game is over somehow
+      // -------------------------------mutation to update eventComplete (ended_at in db)
 
-    console.log('currentRound = ', currentRound)
+      const numNullPairings = newPairings.reduce((all, item, index) => {
+        if (item.indexOf(null) > -1) {
+          all += 1
+        }
+        return all
+      }, 0)
 
-    clearTimeout(roundsTimeout)
-    roundsTimeout = setTimeout(
-      () => runEvent(req, res, currentRound + 1, betweenRoundsTimeout, roundsTimeout),
-      round_length
-    )
-  }, delayBetweenRounds)
+      if (newPairings.length === 0 || numNullPairings > onlineEventUsers.length / 2) {
+        return setTimeout(() => {
+          endEvent(eventId, betweenRoundsTimeout, roundsTimeout)
+        }, roundInterval / 2)
+      }
+
+      // insert data for given round
+      // maybe a .map would be cleaner here?
+      newPairings.forEach((pairing) => {
+        variablesArr.push({
+          partnerX_id: pairing[0],
+          partnerY_id: pairing[1],
+          round_number: currentRound + 1,
+          event_id: eventId,
+        })
+      })
+
+      // insert new pairings result into db
+      try {
+        await orm.request(bulkInsertRounds, {
+          objects: variablesArr,
+        })
+      } catch (error) {
+        Sentry.captureException(error)
+        console.log('error inserting rounds data = ', error)
+        clearTimeout(roundsTimeout)
+      }
+
+      // increment current round in events table
+      try {
+        await orm.request(updateEventObject, {
+          id: eventId,
+          newCurrentRound: currentRound + 1,
+          newStatus: 'room-in-progress',
+        })
+        console.log('set room to in-progress')
+        console.log('eventId = ', eventId)
+      } catch (error) {
+        Sentry.captureException(error)
+      }
+
+      console.log('currentRound = ', currentRound)
+
+      clearTimeout(roundsTimeout)
+      roundsTimeout = setTimeout(
+        () => runEvent(req, res, currentRound + 1, betweenRoundsTimeout, roundsTimeout),
+        round_length
+      )
+    }, delayBetweenRounds)
+  }
 }
 
 export default runEvent
