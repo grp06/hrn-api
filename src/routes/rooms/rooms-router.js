a9205bb885ac9c284c8d9a0d0125123d5d1f6ad3
George Pickett <gpickett00@gmail.com>
2020-10-10 14:48:04 -0500

    Remove need to hit backend to get online users from the pre-event component

--- src/routes/rooms/rooms-router.js
+++ src/routes/rooms/rooms-router.js
@@ -12,35 +12,6 @@ const express = require('express')
 const roomsRouter = express.Router()
 const jsonBodyParser = express.json()
 
-roomsRouter.post('/get-online-event-users/:id', jsonBodyParser, async (req, res) => {
-  // after the event status is set to pre-event, all frontend users (subscribed to the event obj)
-  // hit this route
-  const eventId = req.params.id
-  let userIds
-
-  try {
-    // get all online users for this eventId
-    const onlineUsersResponse = await orm.request(getAvailableLobbyUsers, {
-      eventId,
-    })
-
-    if (onlineUsersResponse.errors) {
-      Sentry.captureException(onlineUsersResponse.errors[0].message)
-      throw new Error(onlineUsersResponse.errors[0].message)
-    }
-
-    const onlineUsers = onlineUsersResponse.data.online_event_users
-    userIds = onlineUsers.map((user) => user.id)
-  } catch (error) {
-    console.log('error = ', error)
-    Sentry.captureException(error)
-
-    return res.status(400).json({ error })
-  }
-
-  return res.status(200).json({ data: userIds })
-})
-
 roomsRouter.post('/start-pre-event/:id', jsonBodyParser, async (req, res) => {
   const eventId = req.params.id
 
