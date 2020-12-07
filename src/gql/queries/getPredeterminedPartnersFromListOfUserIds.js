f2ece8a2e8e213029f75ca3b6d3ddadd7ba8e313
George Pickett <gpickett00@gmail.com>
2020-12-07 11:38:41 -0600

    Get predetermined partners but specify by eventId

--- src/gql/queries/getPredeterminedPartnersFromListOfUserIds.js
+++ src/gql/queries/getPredeterminedPartnersFromListOfUserIds.js
@@ -1,14 +1,15 @@
 import gql from 'graphql-tag'
 
+// make sure to only pull for the current eventId
+
 const getPredeterminedPartnersFromListOfUserIds = gql`
-  query getPredeterminedPartnersFromListOfUserIds($userIds: [Int!]) {
-    predetermined_partners(
-      where: { partner_1_id: { _in: $userIds }, _and: { partner_2_id: { _in: $userIds } } }
-    ) {
-      partner_1_id
-      partner_2_id
-    }
+query getPredeterminedPartnersFromListOfUserIds($userIds: [Int!], $eventId: Int!) {
+  predetermined_partners(where: {partner_1_id: {_in: $userIds}, _and: {partner_2_id: {_in: $userIds}, event_id: {_eq: $eventId}}, event_id: {_eq: $eventId}}) {
+    partner_1_id
+    partner_2_id
   }
+}
+
 `
 
 export default getPredeterminedPartnersFromListOfUserIds
