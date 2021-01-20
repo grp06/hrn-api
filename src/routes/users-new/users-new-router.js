76c839d8ce2b37aafdc34fcef43476ba9ff15518
George Pickett <gpickett00@gmail.com>
2021-01-19 19:33:06 -0600

    Move logic for pivot signup stuff to users-new-router

---
+++ src/routes/users-new/users-new-router.js
@@ -0,0 +1,188 @@
+import * as Sentry from '@sentry/node'
+import orm from '../../services/orm'
+import { findUserByEmail, findUserByPhoneNumber } from '../../gql/queries'
+import { signUp, signUpNew } from '../../gql/mutations'
+import { createToken } from '../../extensions/jwtHelper'
+import UsersService from '../users/users-service'
+import { signUpConfirmation } from '../../services/email-service'
+import { hashPassword } from '../../services/auth-service'
+
+const express = require('express')
+
+const usersNewRouter = express.Router()
+const jsonBodyParser = express.json()
+const { NODE_ENV } = require('../../config')
+
+usersNewRouter.post('/', jsonBodyParser, async (req, res) => {
+  const { cash_app, email, name, password, phone_number, role, venmo } = req.body
+  console.log('req.body at root /signup', req.body)
+
+  if (role === 'fan' && !req.body['phone_number'])
+    return res.status(400).json({
+      error: `Missing phone_number in request body`,
+    })
+
+  if (role === 'celeb' && !req.body['venmo'] && !req.body['cash_app'])
+    return res.status(400).json({
+      error: `Missing either venmo or cash_app in request body`,
+    })
+
+  if (role !== 'fan') {
+    for (const field of ['name', 'email', 'password', 'role'])
+      if (!req.body[field]) {
+        return res.status(400).json({
+          error: `Missing '${field}' in request body`,
+        })
+      }
+  }
+
+  // name, email, password validation
+
+  // add logging for these errors?
+
+  const nameError = UsersService.validateName(name)
+  if (nameError) return res.status(400).json({ error: nameError })
+
+  if (role === 'fan') {
+    let existingFan
+    try {
+      const checkPhoneNumberRequest = await orm.request(findUserByPhoneNumber, { phone_number })
+      existingFan = checkPhoneNumberRequest.data.users_new[0]
+      console.log('checkPhoneNumberRequest', checkPhoneNumberRequest)
+
+      if (existingFan) {
+        const message = 'Phone Number already in use'
+        Sentry.captureMessage(message)
+        return res.status(400).json({ error: message })
+      }
+    } catch (error) {
+      Sentry.captureException(error)
+      console.log('error: ', error)
+
+      return res.status(500).json({
+        error,
+      })
+    }
+
+    const userObject = { name, phone_number, role: 'fan' }
+    console.log('userObject ->', { userObject })
+    const variables = { objects: [userObject] }
+    let newFan
+    console.log('🚀 ~ usersNewRouter.post ~ variables', variables)
+
+    // insert user into db
+    try {
+      const insertUserResult = await orm.request(signUpNew, variables)
+      console.log(insertUserResult)
+      newFan = insertUserResult.data.insert_users_new.returning[0]
+      console.log('newFan ->', newFan)
+      //TODO Add signup confirmation with number instead of email
+    } catch (error) {
+      Sentry.captureException(error)
+      return res.status(500).json({
+        error,
+      })
+    }
+
+    // send token and user details
+    __logger.info(`Fan with phone number ${phone_number} created`)
+    try {
+      return res.status(201).json({
+        token: await createToken(newFan, process.env.SECRET),
+        // TODO serializeUser with Phone number instead of email
+      })
+    } catch (error) {
+      Sentry.captureException(error)
+      return res.status(500).json({
+        error,
+      })
+    }
+  }
+
+  if (role !== 'fan') {
+    const emailError = UsersService.validateEmail(email)
+    if (emailError) return res.status(400).json({ error: emailError })
+
+    const passwordError = UsersService.validatePassword(password)
+    if (passwordError) return res.status(400).json({ error: passwordError })
+
+    // check if user with email exists
+    let existingUser
+    try {
+      const checkEmailRequest = await orm.request(findUserByEmail, { email: email })
+      existingUser = checkEmailRequest.data.users[0]
+      console.log('checkEmailRequest', checkEmailRequest)
+
+      if (existingUser) {
+        const message = 'Email already in use'
+        Sentry.captureMessage(message)
+        return res.status(400).json({ error: message })
+      }
+    } catch (error) {
+      Sentry.captureException(error)
+      console.log('error: ', error)
+
+      return res.status(500).json({
+        error,
+      })
+    }
+  }
+
+  // hash the password
+  let hashedPassword
+  try {
+    hashedPassword = await hashPassword(password)
+  } catch (error) {
+    Sentry.captureException(error)
+    return res.status(500).json({
+      error,
+    })
+  }
+
+  const userObject =
+    role === 'celeb'
+      ? { name, email, password: hashedPassword, role, venmo, cash_app }
+      : { name, email, password: hashedPassword, role }
+
+  console.log({ userObject })
+
+  const variables = { objects: [userObject] }
+  let newUser
+  console.log('🚀 ~ usersNewRouter.post ~ variables', variables)
+
+  // insert user into db
+  try {
+    const insertUserResult =
+      role === 'celeb'
+        ? await orm.request(signUpNew, variables)
+        : await orm.request(signUp, variables)
+    console.log(insertUserResult)
+    newUser =
+      role === 'celeb'
+        ? insertUserResult.data.insert_users_new.returning[0]
+        : insertUserResult.data.insert_users.returning[0]
+    console.log('newUser', newUser)
+    signUpConfirmation(newUser)
+  } catch (error) {
+    Sentry.captureException(error)
+    return res.status(500).json({
+      error,
+    })
+  }
+
+  // send token and user details
+  __logger.info(`User with email ${email} created`)
+  try {
+    return res.status(201).json({
+      token: await createToken(newUser, process.env.SECRET),
+      ...UsersService.serializeUser(newUser),
+    })
+  } catch (error) {
+    Sentry.captureException(error)
+    return res.status(500).json({
+      error,
+    })
+  }
+})
+
+module.exports = usersNewRouter
--- src/routes/users/users-router.js
+++ src/routes/users/users-router.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/node'
 import orm from '../../services/orm'
-import { findUserByEmail, findUserByPhoneNumber } from '../../gql/queries'
-import { signUp, signUpNew, updateUserRole } from '../../gql/mutations'
+import { findUserByEmail } from '../../gql/queries'
+import { signUp, updateUserRole } from '../../gql/mutations'
 import { hashPassword } from '../../services/auth-service'
 import { createToken } from '../../extensions/jwtHelper'
 import UsersService from './users-service'
@@ -16,119 +16,11 @@ const jsonBodyParser = express.json()
 const { NODE_ENV } = require('../../config')
 
 usersRouter.post('/', jsonBodyParser, async (req, res) => {
-  const { cash_app, email, name, password, phone_number, role, venmo } = req.body
+  const { email, name, password, role } = req.body
   console.log('req.body at root /signup', req.body)
-
-  if (role === 'fan' && !req.body['phone_number'])
-    return res.status(400).json({
-      error: `Missing phone_number in request body`,
-    })
-
-  if (role === 'celeb' && !req.body['venmo'] && !req.body['cash_app'])
-    return res.status(400).json({
-      error: `Missing either venmo or cash_app in request body`,
-    })
-
-  if (role !== 'fan') {
-    for (const field of ['name', 'email', 'password', 'role'])
-      if (!req.body[field]) {
-        return res.status(400).json({
-          error: `Missing '${field}' in request body`,
-        })
-      }
-  }
-
-  // name, email, password validation
-
-  // add logging for these errors?
 
   const nameError = UsersService.validateName(name)
   if (nameError) return res.status(400).json({ error: nameError })
-
-  if (role === 'fan') {
-    let existingFan
-    try {
-      const checkPhoneNumberRequest = await orm.request(findUserByPhoneNumber, { phone_number })
-      existingFan = checkPhoneNumberRequest.data.users_new[0]
-      console.log('checkPhoneNumberRequest', checkPhoneNumberRequest)
-
-      if (existingFan) {
-        const message = 'Phone Number already in use'
-        Sentry.captureMessage(message)
-        return res.status(400).json({ error: message })
-      }
-    } catch (error) {
-      Sentry.captureException(error)
-      console.log('error: ', error)
-
-      return res.status(500).json({
-        error,
-      })
-    }
-
-    const userObject = { name, phone_number, role: 'fan' }
-    console.log('userObject ->', { userObject })
-    const variables = { objects: [userObject] }
-    let newFan
-    console.log('🚀 ~ usersRouter.post ~ variables', variables)
-
-    // insert user into db
-    try {
-      const insertUserResult = await orm.request(signUpNew, variables)
-      console.log(insertUserResult)
-      newFan = insertUserResult.data.insert_users_new.returning[0]
-      console.log('newFan ->', newFan)
-      //TODO Add signup confirmation with number instead of email
-    } catch (error) {
-      Sentry.captureException(error)
-      return res.status(500).json({
-        error,
-      })
-    }
-
-    // send token and user details
-    __logger.info(`Fan with phone number ${phone_number} created`)
-    try {
-      return res.status(201).json({
-        token: await createToken(newFan, process.env.SECRET),
-        // TODO serializeUser with Phone number instead of email
-      })
-    } catch (error) {
-      Sentry.captureException(error)
-      return res.status(500).json({
-        error,
-      })
-    }
-  }
-
-  if (role !== 'fan') {
-    const emailError = UsersService.validateEmail(email)
-    if (emailError) return res.status(400).json({ error: emailError })
-
-    const passwordError = UsersService.validatePassword(password)
-    if (passwordError) return res.status(400).json({ error: passwordError })
-
-    // check if user with email exists
-    let existingUser
-    try {
-      const checkEmailRequest = await orm.request(findUserByEmail, { email: email })
-      existingUser = checkEmailRequest.data.users[0]
-      console.log('checkEmailRequest', checkEmailRequest)
-
-      if (existingUser) {
-        const message = 'Email already in use'
-        Sentry.captureMessage(message)
-        return res.status(400).json({ error: message })
-      }
-    } catch (error) {
-      Sentry.captureException(error)
-      console.log('error: ', error)
-
-      return res.status(500).json({
-        error,
-      })
-    }
-  }
 
   // hash the password
   let hashedPassword
@@ -141,10 +33,7 @@ usersRouter.post('/', jsonBodyParser, async (req, res) => {
     })
   }
 
-  const userObject =
-    role === 'celeb'
-      ? { name, email, password: hashedPassword, role, venmo, cash_app }
-      : { name, email, password: hashedPassword, role }
+  const userObject = { name, email, password: hashedPassword, role }
 
   console.log({ userObject })
 
@@ -154,15 +43,10 @@ usersRouter.post('/', jsonBodyParser, async (req, res) => {
 
   // insert user into db
   try {
-    const insertUserResult =
-      role === 'celeb'
-        ? await orm.request(signUpNew, variables)
-        : await orm.request(signUp, variables)
+    const insertUserResult = await orm.request(signUp, variables)
+
     console.log(insertUserResult)
-    newUser =
-      role === 'celeb'
-        ? insertUserResult.data.insert_users_new.returning[0]
-        : insertUserResult.data.insert_users.returning[0]
+    newUser = insertUserResult.data.insert_users.returning[0]
     console.log('newUser', newUser)
     signUpConfirmation(newUser)
   } catch (error) {
