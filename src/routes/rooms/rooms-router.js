b1a1b0ba7b7a48d1da7b5167be1c2c041b271c88
George Pickett <gpickett00@gmail.com>
2021-05-10 18:19:36 +0800

    remove pre-event related stuff

--- src/routes/rooms/rooms-router.js
+++ src/routes/rooms/rooms-router.js
@@ -3,7 +3,6 @@ import setRoomsCompleted from './set-rooms-completed'
 import orm from '../../services/orm'
 import { updateEventObject } from '../../gql/mutations'
 import getOnlineUsers from './getOnlineUsers'
-import createPreEventRooms from './createPreEventRooms'
 import nextRound from './nextRound'
 import { getAvailableLobbyUsers } from '../../gql/queries'
 import { endEvent } from './runEventHelpers'
@@ -20,36 +19,6 @@ roomsRouter.post('/end-event/:id', jsonBodyParser, async (req, res) => {
     console.log('error', error)
     Sentry.captureException(error)
   }
-})
-
-roomsRouter.post('/start-pre-event/:id', jsonBodyParser, async (req, res) => {
-  const eventId = req.params.id
-
-  let onlineUsersResponse
-  try {
-    onlineUsersResponse = await orm.request(getAvailableLobbyUsers, {
-      eventId,
-    })
-    console.log('onlineUsersResponse', onlineUsersResponse)
-
-    const maxNumUsersPerRoom = 40
-    const numOnlineUsers = onlineUsersResponse.data.online_event_users.length
-    console.log('numOnlineUsers', numOnlineUsers)
-    const numRooms = Math.ceil(numOnlineUsers / maxNumUsersPerRoom)
-    console.log('numRooms', numRooms)
-    await createPreEventRooms(numRooms, eventId)
-
-    await orm.request(updateEventObject, {
-      id: eventId,
-      newStatus: 'pre-event',
-    })
-  } catch (error) {
-    Sentry.captureException(error)
-    console.log('error = ', error)
-    return res.status(500).json({ message: 'start pre-event failed' })
-  }
-
-  return res.status(200).json({ message: 'pre-event started' })
 })
 
 // api/rooms/start-event/:eventId
