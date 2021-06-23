8b0d1dc2648bdc68bac7a81b2e8501d6ecb43d9a
George Pickett <gpickett00@gmail.com>
2021-06-23 20:24:21 +0800

    query needs to be to the online_room_users view rather than room_users. Sometimes room_users aren't online (and theyd be spectators) ... screws things up

--- src/gql/queries/getRoomUsersByRoomId.js
+++ src/gql/queries/getRoomUsersByRoomId.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 
 const getRoomUsersByRoomId = gql`
   query getRoomUsersByRoomId($roomId: Int!) {
-    room_users(where: { room_id: { _eq: $roomId } }) {
+    online_room_users(where: { room_id: { _eq: $roomId } }) {
       on_stage
       user_id
     }
