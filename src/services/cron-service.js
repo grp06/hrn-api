f5e57a2bbb295579c64da8b68711c32c0a1e04e1
George Pickett <gpickett00@gmail.com>
2021-06-23 12:23:32 +0800

    remove cron job, not needed anymore

--- src/services/cron-service.js
+++ src/services/cron-service.js
@@ -5,38 +5,3 @@ import { getRecentlyCreatedRooms } from '../gql/queries'
 import orm from './orm'
 
 const cron = require('node-cron')
-
-const deleteStagnantRooms = async () => {
-  const threeHoursInMs = 10800000
-  const threeHoursAgoAsTimestamp = new Date(Date.now() - threeHoursInMs).toISOString()
-
-  try {
-    const getRecentRoomsRes = await orm.request(getRecentlyCreatedRooms, {
-      timestamp: threeHoursAgoAsTimestamp,
-    })
-    const { rooms } = getRecentRoomsRes.data
-
-    const getStagnantUnclaimedRooms = (allRooms) => {
-      const twoHoursOld = 1000 * 60 * 60 * 2
-      const now = new Date().getTime()
-      const stagnantRooms = allRooms.filter((room) => {
-        const lastActive = new Date(room.room_mode.updated_at).getTime()
-        return now - lastActive > twoHoursOld
-      })
-      return stagnantRooms
-    }
-
-    const stagnantUnclaimedRoomIds = getStagnantUnclaimedRooms(rooms).map((room) => room.id)
-    console.log('🚀 ~ roomIds to deletee', stagnantUnclaimedRoomIds)
-
-    await orm.request(deleteRooms, {
-      roomIds: stagnantUnclaimedRoomIds,
-    })
-  } catch (error) {
-    console.log('error = ', error)
-  }
-}
-
-cron.schedule('0 0 */3 * * *', async () => {
-  deleteStagnantRooms()
-})
